fa98fe53e796c09ef263cae21ea14065ec3d36da
lilyshowlader <lilyshowlader@gmail.com>
2023-10-05 16:54:03 -0400

    add in content, fix mobile responsiveness

--- src/Pages/IUD.jsx
+++ src/Pages/IUD.jsx
@@ -1,7 +1,74 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './IUD.modules.css'; 
 
 export default function IUD() {
-	return (
-		<div>blah blah</div>
-	)
+  return (
+    <>
+      <div className='centered-heading'>
+        <h1 className='heading'>birth control, for you.</h1>
+      </div>
+
+      <div className='content-container'>
+        <div className='imageContainer'>
+          <p className='bc-heading'>THE IUD</p>
+          <img
+            src='https://i.imgur.com/SKFbB7b.jpg'
+            alt='IUD'
+            className='iudImage'
+          />
+        </div>
+
+        <div className='text-container'>
+          <p className='sub-heading'>what is it?</p>
+          <p className='answer'>
+            An IUD is a tiny device that's put into your uterus to prevent pregnancy. It’s long-term, reversible, and one of the most effective birth control methods out there.
+          </p>
+          <p className='sub-heading'>what does IUD stand for?</p>
+          <p className='answer'>
+            IUD stands for Intrauterine Device (basically: a device inside your uterus). It's a small piece of flexible plastic shaped like a T. Sometimes it’s called an IUC — intrauterine contraception.
+          </p>
+          <p className='sub-heading'>what are the types of IUDs?</p>
+          <p className='answer'>
+            There are 5 different brands of IUDs that are FDA approved for use in the United States:
+            <ul>
+              <li>Paragard</li>
+              <li>Mirena</li>
+              <li>Kyleena</li>
+              <li>Liletta</li>
+              <li>Skyla</li>
+            </ul>
+          </p>
+        </div>
+      </div>
+
+      <div className='content-container'>
+      <div className='text-container-two'>
+        <p className='sub-heading'>These IUDs are divided into 2 types:</p>
+        <p className='answer'>
+          Hormonal IUDs (Mirena, Kyleena, Liletta, and Skyla)
+          <br />
+          &amp;
+          <br />
+          Copper IUDs (Paragard)
+          <br />
+          <br />
+          The Paragard IUD doesn’t have hormones. It’s wrapped in a tiny bit of copper, and it protects you from pregnancy for up to 12 years.
+
+          The Mirena, Kyleena, Liletta, and Skyla IUDs use the hormone progestin to prevent pregnancy. Progestin is very similar to the hormone progesterone that our bodies make naturally. Mirena works for up to 8 years. Kyleena works for up to 5 years. Liletta works for up to 8 years. Skyla works for up to 3 years.
+        </p>
+      </div>
+      </div>
+   
+      <div className='content-container'>
+      <div>
+        <Link to="/">
+          <button className='home-button'>home</button>
+        </Link>
+      </div>
+      </div>
+
+      <div className='endofpage'></div>
+    </>
+  );
 }
